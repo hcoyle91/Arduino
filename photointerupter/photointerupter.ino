2eb2e2ca5eb092a83568403fad2cc2f17d413c33
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2019-11-07 14:12:01 -0500

    Update photointerupter.ino

--- photointerupter/photointerupter.ino
+++ photointerupter/photointerupter.ino
@@ -1,9 +1,9 @@
-const byte ledPin = 13;
-const byte interruptPin = 2;
-volatile byte state = LOW;
+const byte ledPin = 13;       // Defines the led pin as pin 13
+const byte interruptPin = 2;   // Defines the photo interupter pin as pin 2
+volatile byte state = LOW;       
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);      // sets the variable "ledPin" pin to OUTPUT
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
 }
