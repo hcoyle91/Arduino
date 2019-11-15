a03ff2f18fa5c1f13f4276e55c8c7a12ff44f942
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2019-11-15 14:22:16 -0500

    Update Blink LED.ino

--- Blink LED/Blink LED.ino
+++ Blink LED/Blink LED.ino
@@ -8,21 +8,21 @@ int fadeAmount = 5;
 
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  Serial.begin(9600);  //sets up the serial moniter
+  pinMode(led, OUTPUT); 
 }
 
 
 void loop() {
 
-  analogWrite(led, brightness);
+  analogWrite(led, brightness);  //this send the led the siganl that the variable brightness is recieving
 
   
-  brightness = brightness + fadeAmount;
+  brightness = brightness + fadeAmount; //
 
 
   if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+    fadeAmount = -fadeAmount;  
   
 
 
@@ -31,6 +31,6 @@ void loop() {
  
   delay(50);
 
-   Serial.println(brightness);
+   Serial.println(brightness); //prints the brightness value on the serial moniter
 
 }
